196e40f8b82758a5ff4929ab0a227800339b1a3a
Josh Morrow <josh@jcmorrow.com>
2019-07-02 14:01:12 -0400

    Three done

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,44 @@
-const BASE_64_ALPHABET: [char; 64] = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
-    '5', '6', '7', '8', '9', '+', '/',
-];
+use necromancer::*;
+
+mod necromancer;
 
 fn main() {
-    let b64: String =
-        String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
-    let normal: String = String::from("I'm killing your brain like a poisonous mushroom");
+    // let b64: String =
+    //     String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+    // let normal: String = String::from("I'm killing your brain like a poisonous mushroom");
     let hex: String = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     let h: char = 'h';
-    let e: char = 'e';
-    let l: char = 'l';
-    assert_eq!(char_to_binary(h), vec![0, 1, 1, 0, 1, 0, 0, 0]);
-    assert_eq!(char_to_binary(e), vec![0, 1, 1, 0, 0, 1, 0, 1]);
-    assert_eq!(char_to_binary(l), vec![0, 1, 1, 0, 1, 1, 0, 0]);
+    // let e: char = 'e';
+    // let l: char = 'l';
+    // assert_eq!(char_to_binary(h), vec![0, 1, 1, 0, 1, 0, 0, 0]);
+    // assert_eq!(char_to_binary(e), vec![0, 1, 1, 0, 0, 1, 0, 1]);
+    // assert_eq!(char_to_binary(l), vec![0, 1, 1, 0, 1, 1, 0, 0]);
 
-    assert_eq!(binary_to_u8(&char_to_binary(h)), 104);
+    // assert_eq!(binary_to_u8(&char_to_binary(h)), 104);
 
-    assert_eq!(string_to_base_64(normal), b64);
-    assert_eq!(hex_to_base_64(hex), b64);
-}
+    // assert_eq!(string_to_base_64(normal), b64);
+    // assert_eq!(hex_to_base_64(hex), b64);
 
-fn string_to_base_64(s: String) -> String {
-    let mut b64 = String::new();
-    let mut bins: Vec<u8> = Vec::new();
+    // let string1 = "1c0111001f010100061a024b53535009181c";
+    // let string2 = "686974207468652062756c6c277320657965";
 
-    for c in s.chars() {
-        bins.append(&mut char_to_binary(c));
+    // println!("{:?}", &hex_xor(string1, string2));
+
+    let encrypted = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    let mut decrypted = String::new();
+    let mut best_score = 0.0;
+    // println!("{:?}", hex_to_string(encrypted));
+    // println!("{:?}", hex_to_string(encrypted).into_bytes());
+    for mask_char in 39..128 {
+        let mask_vec = vec![mask_char; encrypted.len()];
+        let mask = String::from_utf8(mask_vec.clone()).unwrap();
+        let score = english_score(&hex_xor(encrypted, &string_to_hex(&mask)).into_bytes());
+        if score > best_score {
+            best_score = score;
+            decrypted = hex_xor(encrypted, &string_to_hex(&mask));
+        }
     }
-    for c in bins.chunks(6) {
-        b64.push(BASE_64_ALPHABET[binary_to_u8(c) as usize]);
-    }
-
-    b64
-}
-
-fn hex_to_base_64(s: String) -> String {
-    let mut b64 = String::new();
-    let mut bins: Vec<u8> = Vec::new();
-
-    for c in s.into_bytes().chunks(2) {
-        bins.append(&mut char_to_binary(
-            u32::from_str_radix(&String::from_utf8(c.to_vec()).unwrap(), 16).unwrap() as u8 as char,
-        ));
-    }
-
-    for c in bins.chunks(6) {
-        b64.push(BASE_64_ALPHABET[binary_to_u8(c) as usize]);
-    }
-
-    b64
-}
-
-fn char_to_binary(c: char) -> Vec<u8> {
-    let mut xs: Vec<u8> = Vec::new();
-    let mut quotient: u8 = c as u8;
-    let mut remainder: u8;
-
-    while quotient > 0 {
-        remainder = quotient % 2;
-        quotient /= 2;
-        xs.push(remainder);
-    }
-
-    xs = pad(&xs, 8);
-    xs.reverse();
-    xs
-}
-
-fn binary_to_u8(bs: &[u8]) -> u8 {
-    let mut n: u8 = 0;
-
-    for (i, b) in bs.iter().rev().enumerate() {
-        n += (2 as u8).pow(i as u32) * b;
-    }
-
-    n
-}
-
-fn pad(xs: &[u8], len: usize) -> Vec<u8> {
-    let mut xs = xs.to_vec();
-    while xs.len() < len {
-        xs.push(0);
-    }
-    xs
+    println!("Sentence: {:?}", decrypted);
+    println!("Score: {:?}", best_score);
 }
---
+++ src/necromancer/mod.rs
@@ -0,0 +1,156 @@
+const WASHINGTON: &str = "On the 9th. William Findley and David Redick--deputed by the Committee of Safety (as it is designated) which met on the 2d. of this month at Parkinson Ferry arrived in Camp with the Resolutions of the said Committee; and to give information of the State of things in the four Western Counties of Pennsylvania to wit--Washington Fayette Westd. & Alligany in order to see if it would prevent the March of the Army into them.  At 10 oclock I had a meeting with these persons in presence of Govr. Howell (of New Jersey) the Secretary of the Treasury, Colo. Hamilton, & Mr.  Dandridge: Govr. Mifflin was invited to be present, but excused himself on acct. of business.  I told the Deputies that by one of the Resolutions it would appear that they were empowered to give information of the disposition & of the existing state of matters in the four Counties above men[tioned]; that I was ready to hear & would listen patiently, and with candour to what they had to say.  Mr. Findley began. He confined his information to such parts of the four Counties as he was best acquainted with; referring to Mr. Reddick for a recital of what fell within his knowledge, in the other parts of these Counties.  The substance of Mr. Findleys communications were as follows--viz.--That the People in the parts where he was best acquainted, had seen there folly; and he believed were disposed to submit to the Laws; that he thought, but could not undertake to be responsible, for the re-establishment of the public Offices for the Collection of the Taxes on distilled spirits, & Stills--intimating however, that it might be best for the present, & until the peoples minds were a little more tranquilized, to hold the Office of Inspection at Pitsburgh under the protection--or at least under the influence of the Garrison; That he thought the Distillers would either enter their stills or would put them down; That the Civil authority was beginning to recover its tone; & enumerated some instances of it; That the ignorance, & general want of information among the people far exceeded any thing he had any conception of; That it was not merely the excise law their opposition was aimed at, but to all law, & Government; and to the Officers of Government; and that the situation in which he had been, & the life he had led for sometime, was such, that rather than go through it again, he would prefer quitting this scene altogether.  Mr. Redicks information was similar to the above; except as to the three last recitals--on wch. I do not recollect that he expressed any sentiment further than that the situation of those who were not in the opposition to government whilst the frenzy was at its height, were obliged to sleep with their Arms by their bed Sides every night; not knowing but that before Morning they might have occasion to use them in defence of their persons, or their properties.  He added, that for a long time after the riots commenced, and until lately, the distrust of one another was such, that even friends were affraid to communicate their sentiments to each other; That by whispers this was brought about; and growing bolder as they became more communicative they found their strength, and that there was a general disposition not only to acquiesce under, but to support the Laws--and he gave some instances also of Magistrates enforcing them.  He said the People of those Counties believed that the opposition to the Excise law--or at least that their dereliction to it, in every other part of the U. States was similar to their own, and that no Troops could be got to March against them for the purpose of coercion; that every acct.  until very lately, of Troops marching against them was disbelieved; & supposed to be the fabricated tales of governmental men; That now they had got alarmed; That many were disposing of their property at an under rate, in order to leave the Country, and added (I think) that they wd. go to Detroit. That no person of any consequence, except one, but what had availed themselves of the proffered amnesty; That those who were still in the opposition, and obnoxious to the laws, were Men of little or no property, & cared but little where they resided; That he did not believe there was the least intention in them to oppose the Army; & that there was not three rounds of ammunition for them in all the Western Country. He (& I think Mr. Findley also) was apprehensive that the resentments of the Army might be productive of treatment to some of these people that might be attended with disagreeable consequences; & on that account seemed to deprecate the March of it: declaring however, that it was their wish, if the people did not give proofs of unequivocal submission, that it might not stop short of its object.  After hearing what both had to say, I briefly told them--That it had been the earnest wish of governmt. to bring the people of those counties to a sense of their duty, by mild, & lenient means; That for the purpose of representing to their sober reflection the fatal consequences of such conduct Commissioners had been sent amongst them that they might be warned, in time, of what must follow, if they persevered in their opposition to the laws; but that coercion wou'd not be resorted to except in the dernier resort: but, that the season of the year made it indispensible that preparation for it should keep pace with the propositions that had been made; That it was unnecessary for me to enumerate the transactions of those people (as they related to the proceedings of government) forasmuch as they knew them as well as I did; That the measure which they were not witness to the adoption of was not less painful than expensive--Was inconvenient, & distressing--in every point of view; but as I considered the support of the Laws as an object of the first magnitude, and the greatest part of the expense had already been incurred, that nothing Short of the most unequivocal proofs of absolute Submission should retard the March of the army into the Western counties, in order to convince them that the government could, & would enforce obedience to the laws--not suffering them to be insulted with impunity. Being asked again what proofs would be required, I answered, they knew as well as I did, what was due to justice & example. They understood my meaning--and asked if they might have another interview. I appointed five oclock in the After noon for it. At this second Meeting there was little more than a repeti[ti]on of what had passed in the forenoon; and it being again mentioned that all the principal characters, except one, in the Western counties who had been in the opposition, had submitted to the propositions--I was induced, seeing them in the Street the next day, to ask Mr. Redick who that one was?--telling him at the same time I required no disclosure that he did not feel himself entirely free to make. He requested a little time to think of it, and asked for another meeting--which was appointed at 5 oclock that afternoon--which took place accordingly when he said David Bradford was the person he had alluded to in his former conversations.  He requested to know if a Meeting of the people, by their deputies, would be permitted by the Army at any given point, on their March into that Country (with fresh evidence of the sincerity of their disposition to acquiesce in whatever might be required) . I replied I saw no objection to it, provided they came unarmed; but to be cautious that not a gun was fired, as there could be no answering for consequences in this case. I assured them that every possible care should be taken to keep the Troops from offering them any insult or damage and that those who always had been subordinate to the Laws, & such as had availed themselves of the amnesty, should not be injured in their persons or property; and that the treatment of the rest would depend upon their own conduct. That the Army, unless opposed, did not mean to act as executioners, or bring offenders to a Military Tribunal; but merely to aid the civil Magistrates, with whom offences would lye. Thus endd. the matter.";
+const BASE_64_ALPHABET: [char; 64] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9', '+', '/',
+];
+
+pub fn string_to_hex_u8(s: &str) -> Vec<u8> {
+    let mut r: Vec<u8> = Vec::new();
+    for hunk in String::from(s).into_bytes().chunks(2) {
+        r.push(hex_to_u8(hunk));
+    }
+    return r;
+}
+
+pub fn hex_to_base_64(s: String) -> String {
+    let mut b64 = String::new();
+    let mut bins: Vec<u8> = Vec::new();
+
+    for c in s.into_bytes().chunks(2) {
+        let hex_string = match String::from_utf8(c.to_vec()) {
+            Ok(x) => x,
+            Err(x) => panic!("Tried to parse hex into utf8, got error: {:?}", x),
+        };
+        let n: u32 = match u32::from_str_radix(&hex_string, 16) {
+            Ok(x) => x,
+            Err(x) => panic!("Tried to parse hex into utf8, got error: {:?}", x),
+        };
+
+        bins.append(&mut char_to_binary(n as u8 as char));
+    }
+
+    for c in bins.chunks(6) {
+        b64.push(BASE_64_ALPHABET[binary_to_u8(c) as usize]);
+    }
+
+    b64
+}
+
+pub fn char_to_binary(c: char) -> Vec<u8> {
+    let mut xs: Vec<u8> = Vec::new();
+    let mut quotient: u8 = c as u8;
+    let mut remainder: u8;
+
+    while quotient > 0 {
+        remainder = quotient % 2;
+        quotient /= 2;
+        xs.push(remainder);
+    }
+
+    xs = pad(&xs, 8);
+    xs.reverse();
+    xs
+}
+
+pub fn binary_to_u8(bs: &[u8]) -> u8 {
+    let mut n: u8 = 0;
+
+    for (i, b) in bs.iter().rev().enumerate() {
+        n += (2 as u8).pow(i as u32) * b;
+    }
+
+    n
+}
+
+pub fn pad(xs: &[u8], len: usize) -> Vec<u8> {
+    let mut xs = xs.to_vec();
+    while xs.len() < len {
+        xs.push(0);
+    }
+    xs
+}
+
+pub fn string_to_base_64(s: String) -> String {
+    let mut b64 = String::new();
+    let mut bins: Vec<u8> = Vec::new();
+
+    for c in s.chars() {
+        bins.append(&mut char_to_binary(c));
+    }
+    for c in bins.chunks(6) {
+        b64.push(BASE_64_ALPHABET[binary_to_u8(c) as usize]);
+    }
+
+    b64
+}
+
+// FF -> 255
+pub fn hex_to_u8(hex: &[u8]) -> u8 {
+    let base_16 = match String::from_utf8(hex.to_vec()) {
+        Ok(x) => x,
+        Err(x) => panic!("Tried to convert {:?} in base-16 to base-10"),
+    };
+    let base_10 = match u32::from_str_radix(&base_16, 16) {
+        Ok(x) => x,
+        Err(x) => panic!(
+            "Tried to convert {:?} in base-16 to base-10",
+            String::from_utf8(hex.to_owned())
+        ),
+    };
+    base_10 as u8
+}
+
+pub fn hex_xor(s1: &str, s2: &str) -> String {
+    let mut xor: Vec<u8> = Vec::new();
+    let hex1 = string_to_hex_u8(&s1);
+    let hex2 = string_to_hex_u8(&s2);
+    for hunks in hex1.iter().zip(hex2) {
+        xor.push(hunks.0 ^ hunks.1);
+    }
+    String::from_utf8(xor).unwrap()
+}
+
+pub fn u8_to_hex(b: &u8) -> String {
+    format!("{:X}", b)
+}
+
+pub fn string_to_hex(s: &str) -> String {
+    String::from(s).into_bytes().iter().map(u8_to_hex).collect()
+}
+
+pub fn hex_to_string(hex: &str) -> String {
+    String::from_utf8(
+        String::from(hex)
+            .into_bytes()
+            .chunks(2)
+            .map(hex_to_u8)
+            .collect(),
+    )
+    .unwrap()
+}
+
+pub fn character_frequencies(chars: &[u8]) -> [u32; 255] {
+    let mut freq = [0 as u32; 255];
+    for ch in chars {
+        freq[*ch as usize] += 1;
+    }
+    let freq_vec: Vec<u32> = freq.iter().map(|x| *x * 100 / chars.len() as u32).collect();
+
+    freq.copy_from_slice(&freq_vec[..255]);
+    freq
+}
+
+pub fn mse(a: u32, b: u32) -> u32 {
+    (a as i32 - b as i32).pow(2) as u32
+}
+
+pub fn english_score(string: &[u8]) -> f32 {
+    let error: u32 = character_frequencies(string)
+        .iter()
+        .zip(character_frequencies(&String::from(WASHINGTON).into_bytes()).iter())
+        .map(|x| mse(*x.0, *x.1 as u32))
+        .sum();
+    10000.0 / error as f32
+}
